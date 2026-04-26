557b882ff2a02619b813ab7728edc7e788d6eeda
Ranjana Singh <ranjanasingh04@gmail.com>
2026-04-26 11:06:48 +0000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     counter = len(sys.argv) -1
 
     if counter == 0:
-        print("0 argument.")
+        print("0 arguments.")
     elif counter == 1:
         print("1 argument:")
     else:
