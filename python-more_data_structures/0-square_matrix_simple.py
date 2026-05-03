b46b609948c9405d1cae8552f2e81f220ab73435
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-03 14:48:54 +0000

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,6 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         new_num = []
         for num in row:
-            new_num.append(num **2)
+            new_num.append(num ** 2)
         new_matrix.append(new_num)
     return new_matrix
