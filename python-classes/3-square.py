8d4f9071acedca52c7a7b8424e319a15db48fca3
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-05 09:37:13 +0000

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,20 +1,19 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """ Module that calculate area of square"""
 
 
 class Square:
     """ Square Class """
-    def __init__(self, _size=0):
+
+    def __init__(self, size=0):
         """ Initiale Square to calculare area"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        
         if size < 0:
             raise ValueError("size must be >= 0")
-        
-        self._size = size
-        
-        def area(self):
-            """ Calculare area of square"""
-            return self.__size ** 2
 
+        self.__size = size
+
+    def area(self):
+        """ Calculare area of square"""
+        return self.__size ** 2
