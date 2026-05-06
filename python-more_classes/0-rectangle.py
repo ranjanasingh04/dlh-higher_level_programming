30860120c82941e41befae3834870fcdafeab735
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-06 11:56:50 +0000

    Emplty Rectangle Module

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-
 class Rectangle:
-    """ Empty Rectangle class"""
-    def __init__(self):
-        """Initialize class"""
-        pass
+    """Defines an empty rectangle."""
+    pass
