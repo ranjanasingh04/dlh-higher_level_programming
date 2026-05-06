8cf614f7317d5b0c7028f8a37bb3e65917aa1120
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-06 07:14:07 +0000

    100-singly_linked_list.py

---
+++ python-classes/100-singly_linked_list.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+class Node:
+    """ Single linkes List Module class"""
+
+    def __init__(self, data,  next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if not isinstance(value, int):
+            raise TypeError("data must be an integer")
+
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and type(value) is not Node:
+            raise TypeError("next_node must be a Node object")
+        self.__next_node = value
+
+
+class SinglyLinkedList:
+    """ Define single linked list"""
+
+    def __init__(self):
+        self.__head = None
+
+    def __str__(self):
+        values = []
+        current = self.__head
+
+        while current is not None:
+            values.append(str(current.data))
+            current = current.next_node
+
+        return "\n".join(values)
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.__head = new_node
+            return
+
+        current = self.__head
+
+        while current.next_node is not None and current.next_node.data < value:
+            current = current.next_node
+
+        new_node.next_node = current.next_node
+        current.next_node = new_node
