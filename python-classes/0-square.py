3bfd28d994407f3e8c2e4f77c7d770eb67692d3c
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-06 08:20:33 +0000

    Testing checker

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module that defines a Square class."""
+"""Defines an empty Square class."""
 
 
 class Square:
-    """Defines a square."""
+    """Empty Square class."""
     pass
