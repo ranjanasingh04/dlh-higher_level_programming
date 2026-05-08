0405cef9618b92b5f1c0e94b026d90fef44290ae
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-08 07:54:28 +0000

    No of instances

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class"""
+
+
+class Rectangle:
+    """print no. of the rectangle Instances"""
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        type(self).number_of_instances += 1
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieve width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set width with validation"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height with validation"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Calculate Area"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Calculate Perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Print resctangle #"""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rows = []
+
+        for i in range(self.__height):
+            rows.append("#" * self.__width)
+
+        return "\n".join(rows)
+
+    def __repr__(self):
+        """Return string representation to recreate object"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Print message when instance is deleted"""
+        type(self).number_of_instances -= 1
+        print("Bye rectangle...")
+
+        
