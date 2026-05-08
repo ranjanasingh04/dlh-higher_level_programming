969f291ad4b1f0bcd3a5f3c232e2b9a5b661634d
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-08 09:44:31 +0000

    append file

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Append file"""
+
+
 def append_write(filename="", text=""):
     """Append text to a UTF-8 file and return number of characters added"""
-    with open(filename, "a", encoding = "utf-8") as f:
+    with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
