060ee200c192e8dba984c9f24eb3b02b57ab666d
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-08 13:22:58 +0000

    Student to JSON

---
+++ python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Student to JSON"""
+
+class Student:
+    """Student to JSON"""
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        return self.__dict__
