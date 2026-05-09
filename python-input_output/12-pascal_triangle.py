66f15c1eb1fec6fe7c2220bbcf3906b2fc1f4511
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-09 16:47:49 +0000

    Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Pascal triangle module"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers
+    representing Pascal's triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        row = [1]  # First element is always 1
+
+        if i > 0:
+            prev_row = triangle[i - 1]
+
+            for j in range(1, i):
+                row.append(prev_row[j - 1] + prev_row[j])
+
+            row.append(1)  # Last element is always 1
+
+        triangle.append(row)
+
+    return triangle
