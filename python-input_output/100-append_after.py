93aaad66b6e424e15f75a6d9d37e83e9ce3eb32e
Ranjana Singh <ranjanasingh04@gmail.com>
2026-05-09 16:44:15 +0000

    updated style Search and update

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 """ Module to Search and update"""
 
+
 def append_after(filename="", search_string="", new_string=""):
     """ append after"""
     updated_lines = []
     with open(filename, "r", encoding="utf-8") as f:
         lines = f.readlines()
 
-        #Process lines
+        # Process lines
         for line in lines:
             updated_lines.append(line)
-        
+
             if search_string in line:
                 updated_lines.append(new_string)
-        
+
     # Rewrite file with updated content
     with open(filename, "w", encoding="utf-8") as f:
         f.writelines(updated_lines)
